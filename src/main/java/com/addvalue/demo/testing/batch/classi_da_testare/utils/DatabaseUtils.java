74c5d98dd19bf2ae07be0b85d06bbc047fbe4ba8
Marco Signorini <marco.signorini@addvalue.it>
2023-05-18 18:01:34 +0200

    Unit Test (pt1)

--- src/main/java/com/addvalue/demo/testing/batch/classi_da_testare/utils/DatabaseUtils.java
+++ src/main/java/com/addvalue/demo/testing/batch/classi_da_testare/utils/DatabaseUtils.java
@@ -1,49 +1,57 @@
 package com.addvalue.demo.testing.batch.classi_da_testare.utils;
 
+import com.addvalue.demo.testing.batch.exceptions.TestingException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.Objects;
 import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 
 @UtilityClass
 public class DatabaseUtils {
 
-  public static String recuperaStringa(ResultSet resultSet, String nomeColonna)
-      throws SQLException {
+  public static String recuperaStringa(ResultSet resultSet, String nomeColonna) {
 
-    return campoValorizzato(resultSet, nomeColonna, String.class)
-        ? StringUtils.trimToEmpty(resultSet.getString(nomeColonna))
-        : "";
+    try {
+
+      final String valore = resultSet.getString(nomeColonna);
+
+      return StringUtils.trimToEmpty(valore);
+    } catch (SQLException e) {
+      throw new TestingException(ExceptionUtils.getMessage(e));
+    }
+
   }
 
-  public static BigDecimal recuperaBigDecimal(
-      ResultSet resultSet, String nomeColonna, int numeroCifreDecimali) throws SQLException {
+  public static BigDecimal recuperaBigDecimal(ResultSet resultSet, String nomeColonna,
+      int numeroCifreDecimali) {
 
-    return campoValorizzato(resultSet, nomeColonna, BigDecimal.class)
-        ? resultSet.getBigDecimal(nomeColonna).setScale(numeroCifreDecimali, RoundingMode.HALF_UP)
-        : BigDecimal.ZERO;
-  }
+    try {
 
-  public static LocalDate recuperaData(ResultSet resultSet, String nomeColonna)
-      throws SQLException {
+      final BigDecimal valore = resultSet.getBigDecimal(nomeColonna);
 
-    return campoValorizzato(resultSet, nomeColonna, LocalDate.class)
-        ? resultSet.getDate(nomeColonna).toLocalDate()
-        : LocalDate.of(1, 1, 1);
-  }
-
-  private static boolean campoValorizzato(ResultSet resultSet, String nomeColonna, Class<?> classe)
-      throws SQLException {
-    if (String.class.equals(classe)) {
-      return StringUtils.isNotBlank(resultSet.getString(nomeColonna));
-    } else if (BigDecimal.class.equals(classe)) {
-      return Objects.nonNull(resultSet.getBigDecimal(nomeColonna));
-    } else {
-      return LocalDate.class.equals(classe) && Objects.nonNull(resultSet.getDate(nomeColonna));
+      return Objects.nonNull(valore) ? valore.setScale(numeroCifreDecimali, RoundingMode.HALF_UP)
+          : BigDecimal.ZERO.setScale(numeroCifreDecimali, RoundingMode.HALF_UP);
+    } catch (SQLException e) {
+      throw new TestingException(ExceptionUtils.getMessage(e));
     }
   }
+
+  public static LocalDate recuperaData(ResultSet resultSet, String nomeColonna) {
+
+    try {
+
+      final Date valore = resultSet.getDate(nomeColonna);
+
+      return Objects.nonNull(valore) ? valore.toLocalDate() : LocalDate.of(1, 1, 1);
+    } catch (SQLException e) {
+      throw new TestingException(ExceptionUtils.getMessage(e));
+    }
+  }
+
 }
