9d59f7142f59fc3783a08887d5e2d5e3bdea632e
Marco Signorini <marco.signorini@addvalue.it>
2023-05-22 21:23:06 +0200

    Unit test (pt finale)

--- src/test/java/com/addvalue/demo/testing/batch/classi_da_testare/tasklet/ProduzioneResocontoAssenzeTaskletTest.java
+++ src/test/java/com/addvalue/demo/testing/batch/classi_da_testare/tasklet/ProduzioneResocontoAssenzeTaskletTest.java
@@ -4,20 +4,25 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyString;
 
 import com.addvalue.demo.testing.batch.classi_da_testare.service.DipendenteService;
+import com.addvalue.demo.testing.batch.classi_da_testare.utils.ScritturaFileUtils;
 import com.addvalue.demo.testing.batch.domains.Assenza;
 import com.addvalue.demo.testing.batch.domains.Dipendente;
 import com.addvalue.demo.testing.batch.exceptions.TestingException;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Collections;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -57,6 +62,35 @@ class ProduzioneResocontoAssenzeTaskletTest {
   }
 
   @Test
+  void execute_laScritturaNelFileGeneraUnErrore_vieneGenerataUnaTestingException() {
+    Mockito.when(namedParameterJdbcTemplate.query(anyString(), any(RowMapper.class)))
+        .thenReturn(
+            Collections.singletonList(
+                new Assenza(
+                    new Dipendente("ABC", "MARCO", "SIGNORINI"), LocalDate.of(2023, 5, 5))));
+    Mockito.when(dipendenteService.ottieniDipendenteDaMatricola("ABC"))
+        .thenReturn(new Dipendente("ABC", "MARCO", "SIGNORINI"));
+
+    try (MockedStatic<ScritturaFileUtils> scritturaFileUtilsMockedStatic =
+        Mockito.mockStatic(ScritturaFileUtils.class)) {
+      scritturaFileUtilsMockedStatic
+          .when(() -> ScritturaFileUtils.ottieniFileSuCuiScrivere(anyString(), anyString()))
+          .thenReturn(new File(directoryTemporanea.getAbsolutePath() + "/mioFile.csv"));
+      try (MockedStatic<FileUtils> fileUtilsMockedStatic = Mockito.mockStatic(FileUtils.class)) {
+        fileUtilsMockedStatic
+            .when(
+                () ->
+                    FileUtils.writeStringToFile(
+                        any(), anyString(), any(Charset.class), anyBoolean()))
+            .thenThrow(IOException.class);
+
+        assertThrows(
+            TestingException.class, () -> produzioneResocontoAssenzeTasklet.execute(null, null));
+      }
+    }
+  }
+
+  @Test
   void execute_nessunaAssenzaTrovata_vieneScrittoIlFileVuotoELaTaskletTerminaCorrettamente()
       throws IOException {
     Mockito.when(namedParameterJdbcTemplate.query(anyString(), any(RowMapper.class)))
@@ -93,6 +127,103 @@ class ProduzioneResocontoAssenzeTaskletTest {
             ProduzioneResocontoAssenzeTasklet.HEADER_FILE_RESOCONTO_ASSENZE
                 + System.lineSeparator()
                 + "ABC;MARCO;SIGNORINI;2023-05-05"
+                + System.lineSeparator());
+    assertThat(repeatStatus).isEqualTo(RepeatStatus.FINISHED);
+  }
+
+  @Test
+  void
+      execute_piuAssenzePerUnDipendenteTrovate_vieneScrittoIlFileCorrettoELaTaskletTerminaCorrettamente()
+          throws IOException {
+    Mockito.when(namedParameterJdbcTemplate.query(anyString(), any(RowMapper.class)))
+        .thenReturn(
+            Arrays.asList(
+                new Assenza(new Dipendente("ABC", "MARCO", "SIGNORINI"), LocalDate.of(2023, 5, 5)),
+                new Assenza(
+                    new Dipendente("ABC", "MARCO", "SIGNORINI"), LocalDate.of(2023, 5, 6))));
+    Mockito.when(dipendenteService.ottieniDipendenteDaMatricola("ABC"))
+        .thenReturn(new Dipendente("ABC", "MARCO", "SIGNORINI"));
+    Mockito.when(dipendenteService.ottieniDipendenteDaMatricola("DEF"))
+        .thenReturn(new Dipendente("DEF", "MARIO", "ROSSI"));
+
+    final RepeatStatus repeatStatus = produzioneResocontoAssenzeTasklet.execute(null, null);
+
+    assertThat(
+            FileUtils.readFileToString(
+                new File(directoryTemporanea.getAbsolutePath() + "/mioFile.csv"), UTF_8))
+        .isEqualTo(
+            ProduzioneResocontoAssenzeTasklet.HEADER_FILE_RESOCONTO_ASSENZE
+                + System.lineSeparator()
+                + "ABC;MARCO;SIGNORINI;2023-05-05"
+                + System.lineSeparator()
+                + "ABC;MARCO;SIGNORINI;2023-05-06"
+                + System.lineSeparator());
+    assertThat(repeatStatus).isEqualTo(RepeatStatus.FINISHED);
+  }
+
+  @Test
+  void
+      execute_unAssenzaPerPiuDipendentiTrovate_vieneScrittoIlFileCorrettoELaTaskletTerminaCorrettamente()
+          throws IOException {
+    Mockito.when(namedParameterJdbcTemplate.query(anyString(), any(RowMapper.class)))
+        .thenReturn(
+            Arrays.asList(
+                new Assenza(new Dipendente("ABC", "MARCO", "SIGNORINI"), LocalDate.of(2023, 5, 5)),
+                new Assenza(new Dipendente("DEF", "MARIO", "ROSSI"), LocalDate.of(2022, 2, 2))));
+    Mockito.when(dipendenteService.ottieniDipendenteDaMatricola("ABC"))
+        .thenReturn(new Dipendente("ABC", "MARCO", "SIGNORINI"));
+    Mockito.when(dipendenteService.ottieniDipendenteDaMatricola("DEF"))
+        .thenReturn(new Dipendente("DEF", "MARIO", "ROSSI"));
+
+    final RepeatStatus repeatStatus = produzioneResocontoAssenzeTasklet.execute(null, null);
+
+    assertThat(
+            FileUtils.readFileToString(
+                new File(directoryTemporanea.getAbsolutePath() + "/mioFile.csv"), UTF_8))
+        .isEqualTo(
+            ProduzioneResocontoAssenzeTasklet.HEADER_FILE_RESOCONTO_ASSENZE
+                + System.lineSeparator()
+                + "ABC;MARCO;SIGNORINI;2023-05-05"
+                + System.lineSeparator()
+                + "DEF;MARIO;ROSSI;2022-02-02"
+                + System.lineSeparator());
+    assertThat(repeatStatus).isEqualTo(RepeatStatus.FINISHED);
+  }
+
+  @Test
+  void
+      execute_piuAssenzePerPiuDipendentiTrovate_vieneScrittoIlFileCorrettoELaTaskletTerminaCorrettamente()
+          throws IOException {
+    Mockito.when(namedParameterJdbcTemplate.query(anyString(), any(RowMapper.class)))
+        .thenReturn(
+            Arrays.asList(
+                new Assenza(new Dipendente("ABC", "MARCO", "SIGNORINI"), LocalDate.of(2023, 5, 5)),
+                new Assenza(new Dipendente("ABC", "MARCO", "SIGNORINI"), LocalDate.of(2023, 5, 6)),
+                new Assenza(new Dipendente("DEF", "MARIO", "ROSSI"), LocalDate.of(2022, 2, 2)),
+                new Assenza(new Dipendente("DEF", "MARIO", "ROSSI"), LocalDate.of(2022, 2, 3)),
+                new Assenza(new Dipendente("DEF", "MARIO", "ROSSI"), LocalDate.of(2022, 2, 4))));
+    Mockito.when(dipendenteService.ottieniDipendenteDaMatricola("ABC"))
+        .thenReturn(new Dipendente("ABC", "MARCO", "SIGNORINI"));
+    Mockito.when(dipendenteService.ottieniDipendenteDaMatricola("DEF"))
+        .thenReturn(new Dipendente("DEF", "MARIO", "ROSSI"));
+
+    final RepeatStatus repeatStatus = produzioneResocontoAssenzeTasklet.execute(null, null);
+
+    assertThat(
+            FileUtils.readFileToString(
+                new File(directoryTemporanea.getAbsolutePath() + "/mioFile.csv"), UTF_8))
+        .isEqualTo(
+            ProduzioneResocontoAssenzeTasklet.HEADER_FILE_RESOCONTO_ASSENZE
+                + System.lineSeparator()
+                + "ABC;MARCO;SIGNORINI;2023-05-05"
+                + System.lineSeparator()
+                + "ABC;MARCO;SIGNORINI;2023-05-06"
+                + System.lineSeparator()
+                + "DEF;MARIO;ROSSI;2022-02-02"
+                + System.lineSeparator()
+                + "DEF;MARIO;ROSSI;2022-02-03"
+                + System.lineSeparator()
+                + "DEF;MARIO;ROSSI;2022-02-04"
                 + System.lineSeparator());
     assertThat(repeatStatus).isEqualTo(RepeatStatus.FINISHED);
   }
